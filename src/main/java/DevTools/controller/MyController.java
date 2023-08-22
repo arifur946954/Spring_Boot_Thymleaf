e76181cbddeea079a7c89b7233f582d073fc915a
Arifur Rahman <arif.bd08060@gmail.com>
2023-08-23 00:06:47 +0600

    thymleaf condition,design part

--- src/main/java/DevTools/controller/MyController.java
+++ src/main/java/DevTools/controller/MyController.java
@@ -1,21 +1,41 @@
 package DevTools.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
 
-     @RequestMapping(value = "/about",method = RequestMethod.GET )
+     @RequestMapping(value = "/abouts",method = RequestMethod.GET )
 	public String about(Model model) {
 		System.out.println("this is about pages");
 		model.addAttribute("name","arifur rahman");
 		model.addAttribute("CurrentDate",new Date().toLocaleString());
 		return "about";
+	}
+     
+     //iteration 
+     @GetMapping("/iterate-example")
+          public String iterationHandle(Model mdl) {
+    	  List<String> name=List.of("Arif","Zarif","sharif","sakib");
+    	  mdl.addAttribute("names",name);//names=name
+    		mdl.addAttribute("name","arifur rahman sfkjhs");//name=arifur rahman
+    	 return "iteration";
+		
+	}
+     @GetMapping("/condition")
+     public String condition(Model m) {
+    
+    	 m.addAttribute("isActive",true);//isActive=true
+    	 m.addAttribute("Gender",'F');
+    	 return "condition";
+		
 	}
 
 }
